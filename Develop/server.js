606de2116d0fe8f335d56a9c9fc6e6bd15c3038a
Robert Harris <rlh9@rice.edu>
2020-03-27 20:34:36 -0500

    Attempting to make program work via heroku

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,14 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api.js")(app);
+require("./routes/html-routes.js")(app);
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+module.exports = app;
