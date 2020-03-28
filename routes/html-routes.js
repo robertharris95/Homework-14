cdc5c0c5a064cfd1eea0611720a5a0cd568fabd2
Robert Harris <rlh9@rice.edu>
2020-03-27 21:51:01 -0500

    added webmanifest and service worker

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = app => {
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 }
