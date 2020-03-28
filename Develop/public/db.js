7be0a684310a360e0355b13984ada89ab1fdbabe
Robert Harris <rlh9@rice.edu>
2020-03-27 19:59:28 -0500

    Functioning as designed

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -3,7 +3,6 @@ let db;
 const request = window.indexedDB.open("budget", 1);
 request.onupgradeneeded = function(event) {
   // create object store called "pending" and set autoIncrement to true
-  const db = event.result;
   const pendingStore = db.createObjectStore("pending", {autoIncrement: true});
 };
 
@@ -21,7 +20,6 @@ request.onerror = function({target}) {
 };
 
 function saveRecord(record) {
-  const db = request.result
   // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(['pending'], 'readwrite')
   // access your pending object store
@@ -31,7 +29,6 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
-  const db = record.result
   // open a transaction on your pending db
   const transaction = db.transaction(['pending'], 'readwrite')
   // access your pending object store
@@ -39,11 +36,11 @@ function checkDatabase() {
   // get all records from store and set to a variable
   const getPending = pendingStore.getAll();
 
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
+  getPending.onsuccess = function() {
+    if (getPending.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
-        body: JSON.stringify(getAll.result),
+        body: JSON.stringify(getPending.result),
         headers: {
           Accept: "application/json, text/plain, */*",
           "Content-Type": "application/json"
@@ -54,6 +51,8 @@ function checkDatabase() {
           // if successful, open a transaction on your pending db
           // access your pending object store
           // clear all items in your store
+          const transaction = db.transaction(['pending'], 'readwrite')
+          const pendingStore = transaction.objectStore('pending')
           pendingStore.clear();
       });
     }
